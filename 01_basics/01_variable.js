0b7ab975328c380207cedfa33b76c750715bc16e
Nishant-Sharma-102 <kaushiknishant2002@gmail.com>
2025-03-15 13:09:04 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 1745
+let emailId ="nishant@gamil.com"
+var passaward = "12345"
+accountCity = "mawana"
+console.log(accountId);
+// accountId = 2 not allowed because const variable doesnot change 
+let AccountState;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+emailId = "kaushiknishant@gmail.com"
+passaward = "7487"
+accountCity = "meerut"
+ 
+
+
+console.log([emailId,passaward,accountCity,AccountState]);
+
+
+
