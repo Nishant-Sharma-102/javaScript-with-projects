e26b231afe173276eb7e712ca28754358fdd6b74
Nishant-Sharma-102 <kaushiknishant2002@gmail.com>
2025-03-15 13:07:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"  // treat all js code as newer version
+ 
+//alert(3+3) // alert we are not using here because is it a node js not a browser
+
+// we make sure that our code is readablity  should be high
+
+
+let Name = "Alash"
+let age = 12
+let isloogedIn = true;
+let state;
+let number  = null
+//number => 2 to power 53
+//bigint
+//string=>
+    //undefined
+//symbol => unique
+//object
+console.log(typeof isloogedIn);
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
