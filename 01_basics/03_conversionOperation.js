d41e1add737e369f5523a41a71db95e6109d7b9e
Nishant-Sharma-102 <kaushiknishant2002@gmail.com>
2025-03-20 16:45:47 +0000

    conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+let score = 23
+//console.log(typeof score)
+let changetype = score
+//console.log(typeof changetype)
+
+// "23" >= 23
+// "23abc" >= NaN
+// true => 1
+// false => 0
+ let boolcheck = "34"
+ let checkboolean = Boolean(boolcheck)
+ //console.log(checkboolean)
+ let again = " "
+ let checkbooleann = Boolean(again)
+ //console.log(checkbooleann)
+ let againn = 1
+ let checkbooleannn = Boolean(againn)
+ //console.log(checkbooleannn)
+ let againnn = 0
+ let checkbooleannnn = Boolean(againnn)
+ //console.log(checkbooleannnn)
+
+//  " " => true;
+//  "nishant" => true;
+//  1 => true;
+//  0 => false;
+
+// *********************Operation****************
+ 
+let value = 5
+let negvalue =-value
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(3%2)
+
+// console.log(2-3)
+// This is simple convrsion
+
+// console.log("2"+2)
+// console.log(2+"2")
+// console.log("2"+"2")
+
+//This automatic convert into number or which is required to get answer
+
+let str1= "Nishant"
+let str2 = " Sharma"
+//console.log(str1+str2)
+
+console.log(2+"2") // 22
+console.log(2+"2"+"2")//222
+
+console.log(2+2+"2")//42
+console.log("2"+"2"+"2")//222
+
+
+let num1, num2, num3
+num1=num2=num3 = 4+4
+console.log(num1,num2,num3)// 888
+
+let counter = 100
+counter++
+console.log(counter) //101
+ ++counter
+ console.log(counter)//101
