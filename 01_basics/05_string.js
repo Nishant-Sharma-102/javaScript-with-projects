b2f20f190928c1ec1b48f11463e19a94432aad76
Nishant-Sharma-102 <kaushiknishant2002@gmail.com>
2025-03-23 02:01:10 +0000

    string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,21 @@
+const name =  "nishant"
+const value  = 19
+const rollno = 67
+
+// console.log(name+" " +value+"age");
+// console.log(`my name is ${name} Sharma and my age is ${value}} 
+//     and i am a student my roll no. is 67 `);//convinent way to addstrings
+
+const method = new String("nishant sharma");
+console.log(method.substring(0,4));
+console.log(method.charAt(3));
+console.log(method.indexOf('t'));
+console.log(method.includes("sh"));
+console.log(method.trim());
+console.log(method.toUpperCase());
+console.log(method.length);
+console.log(method[0]);
+
+
+
+
