b94f5d73d36a52e6e4ddcbab482af5ec85098563
Nishant Sharma <kaushiknishant2002@gmail.com>
2025-06-30 16:36:52 +0000

    03_objects.js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+//Singleton
+//object.creation
+
+//object literals
+
+const mySym = Symbol("key1");
+const jsUser={
+     name: "nishant",
+     [mySym]:"myNameiskey",
+     class:12,
+     email:"nishagmailcom",
+     course:"Btech",
+};
+// console.log(jsUser.name);
+// console.log(jsUser.class);
+// console.log(jsUser["course"]); 
+// console.log(jsUser.class);
+// console.log(jsUser[mySym]);
+
+//Change vallue 
+
+jsUser.email = "vishagmail.com";
+//console.log(jsUser.email);
+
+//Object.freeze(jsUser);
+jsUser.email = "vikkugmail.com";
+//console.log(jsUser);
+
+jsUser.greeting=function(){
+    console.log(`Namaste js ${this.name}`);
+}
+
+console.log(jsUser.greeting());
