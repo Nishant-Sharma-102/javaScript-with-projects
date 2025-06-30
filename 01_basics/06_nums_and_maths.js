b683a11600757f434d93ffd22342f597fa64c2c2
Nishant Sharma <kaushiknishant2002@gmail.com>
2025-06-30 03:25:28 +0000

    06_nums_and_maths.js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,22 +1,15 @@
 // if we want normal random number in math
- console.log(Math.random())
+ //console.log(Math.random())
 
  //with abs value
- console.log(Math.floor(Math.random()*10))
+ //console.log(Math.floor(Math.random()*10))
 
 
+const min  = 10;
+const max = 20;
 
+console.log(Math.floor(Math.random()*(max-min+1))+min)
 
  //NOTE IF WE WANT RANDOM VALUE WITH CERTEIN PERIOD
 
- const min = 10;
- const max = 20;
-
-Math.random()* (max-min+1)
-
-console.log(Math.floor(Math.random()*(max-min+1)+min))
-  
-// THE  output is beetween 10 or 20(include 10 or 20)
-
- 
  
